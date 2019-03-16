a0c474361b2be6c22ae9ef6dbfabd6adffd98c84
santiagox2060 <santiagox2060@gmail.com>
2019-03-16 11:48:25 -0500

    creando variables

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var cero = getElementById('0');
+var uno = getElementById('1');
+var dos = getElementById('2');
+var tres = getElementById('3');
+var cuatro = getElementById('4');
+var cinco = getElementById('5');
+var seis = getElementById('6');
+var siete = getElementById('7');
+var ocho = getElementById('8');
+var nueve = getElementById('9');
+
+var on = getElementById('on');
+var signo = getElementById('sign');
+var raiz = getElementById('raiz');
+var mas = getElementById('mas');
+var menos = getElementById('menos');
+var por = getElementById('por');
+var divide = getElementById('dividido');
+var punto = getElementById('punto');
+var igual = getElementById('igual');
+
+
+var pantalla = getElementById('display');
